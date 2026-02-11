1d5b3596c44105ea54807e5fe5ae8093c2a0e4df
dattvishnu1818@gmail.com <vishsaini18@gmail.com>
2026-02-11 23:28:34 +0530

    added the user checker app files

---
+++ userCheckerApp/app.js
@@ -0,0 +1,32 @@
+const inputId = document.getElementById('inputId')
+const btn = document.getElementById('btn')
+const output = document.getElementById('output')
+
+let user = {
+  name: "",
+};
+
+btn.addEventListener('click', function(){
+
+    // const inputValue = inputId.value
+
+    user.name = inputId.value;
+
+    if(inputId.value ===''){
+        alert('Kindly enter your name')
+    }else if(inputId.value === 'raj'){
+        output.textContent = "Welcome " + user.name;
+        output.style.color = "green";
+        output.style.transition = 'transform 1s ease';
+        output.style.transform = 'rotate(360deg)';
+        output.style.backgroundColor = '#46e06a99'
+        
+    }else {
+            output.textContent = 'User not valid!!';
+            output.style.color = 'red';
+            output.style.transition = 'transform 1s ease';
+            output.style.transform = 'rotate(360deg)';
+            output.style.backgroundColor = '#dd6e6e99'
+    }
+})
+
